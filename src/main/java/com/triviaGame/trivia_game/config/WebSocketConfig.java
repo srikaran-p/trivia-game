6156de1d75e0675be8b8b06085c0d83506f59b55
Srikaran P <srikaran.p@gmail.com>
2026-01-17 23:15:10 +0530

    Configure basic Web Socket

---
+++ src/main/java/com/triviaGame/trivia_game/config/WebSocketConfig.java
@@ -0,0 +1,24 @@
+package com.triviaGame.trivia_game.config;
+
+import com.triviaGame.trivia_game.service.GameWebSocketHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final GameWebSocketHandler handler;
+
+    public WebSocketConfig(GameWebSocketHandler handler) {
+        this.handler = handler;
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(handler, "/ws")
+                .setAllowedOrigins("*");
+    }
+}
---
+++ src/main/java/com/triviaGame/trivia_game/service/GameWebSocketHandler.java
@@ -0,0 +1,29 @@
+package com.triviaGame.trivia_game.service;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component
+public class GameWebSocketHandler extends TextWebSocketHandler {
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        System.out.println("Connected: " + session.getId());
+    }
+
+    @Override
+    public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
+        System.out.println("Received: " + message.getPayload());
+
+        session.sendMessage(new TextMessage("ACK: " + message.getPayload()));
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        System.out.println("Disconnected: " + session.getId());
+    }
+}
